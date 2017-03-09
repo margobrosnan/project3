9072bfaf7341cdbe19f55dcddcb53f34ac71d4b4
Margo Brosnan <mbrosnan@mitre.org>
2017-03-09 14:55:40 -0500

    changes to test branching

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,10 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
+        // Prints a greeting to the terminal window.
         System.out.println("Hello, World!  How are you?\n");
+       // Reply to the greeting on the terminal window.
+        System.out.println("Fine, thank you, and you?\n");
     }
 
 }
